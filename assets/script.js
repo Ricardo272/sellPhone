8d81396f8d65972646b388e3b07a4332f9504d25
unknown <chems.ly@hotmail.fr>
2023-12-13 09:24:01 +0100

    fetch json

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+let cheminImgArticle = ('../assets/img/')
+let article = document.getElementById('article')
+let globalData
+
+
+fetch('../assets/data/article.json')
+.then((res) => res.json())
+  .then((jsonData) => {
+    globalData = jsonData; 
+
+    for (let i = 0; i < globalData.length; i++) {
+article.innerHTML += `
+
+<img src="${cheminImgArticle}${globalData[i].Image}" alt="${globalData[i].Nom}">
+
+`
+
+  }
+
+})
